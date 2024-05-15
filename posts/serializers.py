e22085afd65176cb7327c843c93cd944ff4c2f79
dylan <nyskyline7@gmail.com>
2024-05-15 15:42:09 +0900

    fix: fixed issue where passcode is sent back on /GET

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,3 +5,8 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = "__all__"
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret.pop('passcode', None) #GET요청 받을때 passcode는 빼고 전달해주기!
+        return ret
