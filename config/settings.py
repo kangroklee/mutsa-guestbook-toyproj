ef9342df9b4c879f2f9087f186d11bad0f159630
dylan <nyskyline7@gmail.com>
2024-05-26 22:34:26 +0900

    fix: add fe addr to cors_allowed_origins

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [ 
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOWED_ORIGINS = ["*"]
