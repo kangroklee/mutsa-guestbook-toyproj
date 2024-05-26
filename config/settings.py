16cab1be3e5723918f7f835a757fec95b5901381
dylan <nyskyline7@gmail.com>
2024-05-27 01:11:20 +0900

    fix: remove path in cors_allowed_origins

--- config/settings.py
+++ config/settings.py
@@ -152,4 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [ 
+    "http://taejindylantoyproj.kro.kr",
+    "https://taejindylantoyproj.kro.kr",
+]
