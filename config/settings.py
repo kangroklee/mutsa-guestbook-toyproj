793c54dc1b625ac6ee37ac3a79076196298a1cf3
dylan <nyskyline7@gmail.com>
2024-05-27 01:41:08 +0900

    fix: add localhost to cors_allowed_origins

--- config/settings.py
+++ config/settings.py
@@ -155,4 +155,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://taejindylantoyproj.kro.kr",
     "https://taejindylantoyproj.kro.kr",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
